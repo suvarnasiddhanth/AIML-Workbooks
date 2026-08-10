abf731e6e0b1c90a333e45affceacd863c260c6d
Sid_Heavenward <suvarnasiddhanth@gmail.com>
2026-08-10 12:22:38 +0530

    Regular class problems (updt)

--- AMLWorkbook/Aug10problems.py
+++ AMLWorkbook/Aug10problems.py
@@ -1,7 +1,18 @@
 def create_handle(uid, bank_id):
 	return (uid, bank_id)
-def read_id(uh):
-	return uh[0]
-def read_bank_id(uh):
-	return uh[1]
+def read_id(upihandle):
+	return upihandle[0]
+def read_bank_id(upihandle):
+	return upihandle[1]
+
+class upitx:
+	def __init__():
+		...
+class upipaymentstx(upitx):
+	def __init__(source_handle, reciever_handle, amount):
+		...
+class upireciepttx(upitx):
+	def __init__(source_handle, reciever_handle, amount):
+		...
+
 print(read_id(create_handle(9741001590,"hdfc")))
