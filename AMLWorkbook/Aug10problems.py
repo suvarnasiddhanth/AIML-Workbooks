b660979b288aac2898d2c2a0b98cc780805363f9
Sid_Heavenward <suvarnasiddhanth@gmail.com>
2026-08-10 12:33:44 +0530

    Added more arguments to previous methods

--- AMLWorkbook/Aug10problems.py
+++ AMLWorkbook/Aug10problems.py
@@ -6,13 +6,17 @@ def read_bank_id(upihandle):
 	return upihandle[1]
 
 class upitx:
-	def __init__():
+	def __init__(self, source_handle, reciever_handle, amount):
+		# self.tx_id = ???
+		# self.sender = sender
+		# self.reciever = reciever
+		# self.amount = amount
 		...
 class upipaymentstx(upitx):
-	def __init__(source_handle, reciever_handle, amount):
+	def __init__(self, source_handle, reciever_handle, amount):
 		...
 class upireciepttx(upitx):
-	def __init__(source_handle, reciever_handle, amount):
+	def __init__(self, source_handle, reciever_handle, amount):
 		...
 
 print(read_id(create_handle(9741001590,"hdfc")))
